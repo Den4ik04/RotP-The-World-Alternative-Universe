9b62360b4dd471ec83744942e564d2e77b148f6d
Solkree <densmola303@gmail.com>
2026-06-18 21:35:15 +0300

    Update InitStands.java

--- src/main/java/com/august_dr/rotp_theworld/init/InitStands.java
+++ src/main/java/com/august_dr/rotp_theworld/init/InitStands.java
@@ -107,7 +107,7 @@ public class InitStands {
                     .standWindupDuration(5).standPerformDuration(32).standRecoveryTicks(5)
                     .staminaCost(100).cooldown(800)
                     .resolveLevelToUnlock(2)
-                    .standOffsetFromUser(0.75, 0.75, 0.2)
+                    .standOffsetFromUser(0.0, 1.2, 0.0)
                     .standPose(StandPose.BARRAGE)
                     .partsRequired(StandPart.ARMS)));
 
