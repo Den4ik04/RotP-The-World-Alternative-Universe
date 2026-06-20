67a47a2d681c4d370956f2d40a84429abf3d3fbe
Solkree <densmola303@gmail.com>
2026-06-20 14:22:08 +0300

    Update ClientInit.java

--- src/main/java/com/august_dr/rotp_theworld/client/ClientInit.java
+++ src/main/java/com/august_dr/rotp_theworld/client/ClientInit.java
@@ -1,9 +1,11 @@
 package com.august_dr.rotp_theworld.client;
 
 import com.august_dr.rotp_theworld.RotpTHEWORLDAddon;
+import com.august_dr.rotp_theworld.client.render.entity.renderer.SilverBulletHorseRenderer;
 import com.august_dr.rotp_theworld.client.render.entity.renderer.stand.THEWORLDRenderer;
 import com.august_dr.rotp_theworld.init.AddonStands;
 
+import net.minecraft.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -16,5 +18,6 @@ public class ClientInit {
     @SubscribeEvent
     public static void onFMLClientSetup(FMLClientSetupEvent event) {
         RenderingRegistry.registerEntityRenderingHandler(AddonStands.THE_WORLD.getEntityType(), THEWORLDRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(EntityType.HORSE, SilverBulletHorseRenderer::new);
     }
 }
